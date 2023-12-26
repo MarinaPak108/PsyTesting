8dc64ec7d0c88cebd5973643abd06c4a6b1841f9
pakmarina108@gmail.com <pm@mail.ru>
2023-12-26 13:12:04 +0900

    security corrected

--- src/main/java/com/psytest/config/UserSecurityConfiguration.java
+++ src/main/java/com/psytest/config/UserSecurityConfiguration.java
@@ -27,12 +27,19 @@ public class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/create").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll()
-                .defaultSuccessUrl("/home").permitAll()
-                .failureForwardUrl("/errorlogin").permitAll()
-                .and().logout().permitAll();
+        http.
+                authorizeRequests()
+                    .antMatchers("/create").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/home")
+                    .failureUrl("/errorlogin")
+                    .permitAll()
+                    .and()
+                .logout()
+                .permitAll();
 
         http.csrf().disable();
     }
